50db70e2c425697baef141f48f88e8c5de0b96ba
Adegite Taiwo <adegitetaiwo24@gmail.com>
2022-06-13 04:23:01 +0100

    hide SECRET KEY

--- adegite/adegite/settings.py
+++ adegite/adegite/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()  # take environment variables from .env.
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_!$$v2zng+n2)7dk(7s^pox3#tp6xwoq_sfn2asr^wj4mzh71#'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
